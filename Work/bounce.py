44da96a8a4dc10c55bdf43f4b41771103feb9875
RobCac <roberto.cacciatore@outlook.com>
2023-11-24 13:32:17 +0200

    Started to work on the excersizes and git practice

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
 # bounce.py
 #
 # Exercise 1.5
+ball_height = 100
+bounce = 0
+while bounce < 10:
+    ball_height = ball_height* 0.6
+    bounce += 1
+    print(bounce, round(ball_height, 2))
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,26 @@
 # mortgage.py
 #
 # Exercise 1.7
+# mortgage.py
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+payment_count = 0
+extra_payment_sum = 1000
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+
+while principal > 0:
+    payment_count += 1
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    if payment_count in range(extra_payment_start_month, extra_payment_end_month+1):
+        total_paid = total_paid  + extra_payment_sum
+        principal = principal - extra_payment_sum
+    print(payment_count, total_paid, principal)
+       
+
+print('Total paid', total_paid)
+print(f"Months {payment_count}")
